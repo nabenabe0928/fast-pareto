52c6f17ca3495230349907f4e3bc0b0ea443c189
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2022-04-21 08:04:13 +0900

    [version] Add some info in __init__

--- fast_pareto/__init__.py
+++ fast_pareto/__init__.py
@@ -1,4 +1,12 @@
 from fast_pareto.pareto import is_pareto_front, nondominated_rank
 
 
+__version__ = "0.0.4"
+__copyright__ = "Copyright (C) 2022 Shuhei Watanabe"
+__licence__ = "Apache-2.0 License"
+__author__ = "Shuhei Watanabe"
+__author_email__ = "shuhei.watanabe.utokyo@gmail.com"
+__url__ = "https://github.com/nabenabe0928/fast_pareto"
+
+
 __all__ = ["nondominated_rank", "is_pareto_front"]
