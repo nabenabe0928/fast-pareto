f6dd09a69b150e82953695ff47f787ee65d2acec
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2022-09-12 11:46:24 +0900

    [refactor] Share some process for tie-breaking

--- fast_pareto/pareto.py
+++ fast_pareto/pareto.py
@@ -110,59 +110,6 @@ def is_pareto_front(
     return mask
 
 
-def _tie_break_by_avg_rank(costs: np.ndarray, nd_ranks: np.ndarray) -> np.ndarray:
-    """
-    Tie-break the non-domination ranks (, but we cannot guarantee no duplications)
-
-    Args:
-        costs (np.ndarray):
-            An array of costs (or objectives).
-            This array must be already sorted and must be copied.
-            The shape is (n_observations, n_objectives).
-        nd_ranks (np.ndarray):
-            The non-dominated rank of each observation.
-            The shape is (n_observations, ).
-            The rank starts from zero and lower rank is better.
-
-    Returns:
-        tie_broken_nd_ranks (np.ndarray):
-            The each non-dominated rank will be tie-broken
-            so that we can sort identically (but we may get duplications).
-            The shape is (n_observations, ) and the array is a permutation of zero to n_observations - 1.
-
-    Reference:
-        Paper Title:
-            Techniques for Highly Multiobjective Optimisation: Some Nondominated Points are Better than Others
-        One sentence summary:
-            Average ranking strategy is effective to tie-break in some evolution strategies methods.
-        Authors:
-            David Come and Joshua Knowles
-        URL:
-            https://arxiv.org/pdf/0908.3025.pdf
-    """
-    rank_lb = 0  # the non-domination rank starts from zero
-    rank_ub = nd_ranks.max() + 1
-    masks: List[List[int]] = [[] for _ in range(rank_lb, rank_ub)]
-    for idx, nd_rank in enumerate(nd_ranks):
-        masks[nd_rank].append(idx)
-
-    n_checked = 0
-    ranks = scipy.stats.rankdata(costs, axis=0)
-    # min_ranks_factor plays a role when we tie-break same average ranks
-    min_ranks_factor = np.min(ranks, axis=-1) / (nd_ranks.size**2 + 1)
-    avg_ranks = np.mean(ranks, axis=-1) + min_ranks_factor
-    tie_broken_nd_ranks = np.zeros_like(nd_ranks, dtype=np.int32)
-
-    for nd_rank in range(rank_lb, rank_ub):
-        mask = masks[nd_rank]
-        tie_break_ranks = scipy.stats.rankdata(avg_ranks[mask]).astype(np.int32)
-        # -1 to start tie_broken_nd_ranks from zero
-        tie_broken_nd_ranks[mask] = tie_break_ranks + n_checked - 1
-        n_checked += len(mask)
-
-    return tie_broken_nd_ranks
-
-
 def _compute_rank_based_crowding_distance(ranks: np.ndarray) -> np.ndarray:
     (n_observations, n_obj) = ranks.shape
     order = np.argsort(ranks, axis=0)
@@ -181,21 +128,23 @@ def _compute_rank_based_crowding_distance(ranks: np.ndarray) -> np.ndarray:
     return scipy.stats.rankdata(-dists).astype(np.int32)
 
 
-def _tie_break_by_crowding_distance(
-    costs: np.ndarray, nd_ranks: np.ndarray
+def _tie_break_by_method(
+    nd_masks: List[List[int]],
+    ranks: np.ndarray,
+    avg_ranks: Optional[np.ndarray] = None,
 ) -> np.ndarray:
     """
-    Tie-break the non-domination ranks using the crowding distance in NSGA-II
+    Tie-break the non-domination ranks (, but we cannot guarantee no duplications)
 
     Args:
-        costs (np.ndarray):
-            An array of costs (or objectives).
-            This array must be already sorted and must be copied.
-            The shape is (n_observations, n_objectives).
-        nd_ranks (np.ndarray):
-            The non-dominated rank of each observation.
+        nd_masks (List[List[int]]):
+            The indices of observations in each non-domination rank.
+        ranks (np.ndarray):
+            The ranks of each objective in each observation.
+            The shape is (n_observations, n_obj).
+        avg_ranks (Optional[np.ndarray]):
+            The average rank + small deviation by the best rank of objectives in each observation.
             The shape is (n_observations, ).
-            The rank starts from zero and lower rank is better.
 
     Returns:
         tie_broken_nd_ranks (np.ndarray):
@@ -203,7 +152,17 @@ def _tie_break_by_crowding_distance(
             so that we can sort identically (but we may get duplications).
             The shape is (n_observations, ) and the array is a permutation of zero to n_observations - 1.
 
-    Reference:
+    Reference for avg_rank:
+        Paper Title:
+            Techniques for Highly Multiobjective Optimisation: Some Nondominated Points are Better than Others
+        One sentence summary:
+            Average ranking strategy is effective to tie-break in some evolution strategies methods.
+        Authors:
+            David Come and Joshua Knowles
+        URL:
+            https://arxiv.org/pdf/0908.3025.pdf
+
+    Reference for crowding distance:
         Paper Title:
             A fast and elitist multiobjective genetic algorithm: NSGA-II
         One sentence summary:
@@ -213,18 +172,16 @@ def _tie_break_by_crowding_distance(
         URL:
             http://vision.ucsd.edu/~sagarwal/nsga2.pdf
     """
-    rank_lb = 0  # the non-domination rank starts from zero
-    rank_ub = nd_ranks.max() + 1
-    masks: List[List[int]] = [[] for _ in range(rank_lb, rank_ub)]
-    for idx, nd_rank in enumerate(nd_ranks):
-        masks[nd_rank].append(idx)
-
     n_checked = 0
-    ranks = scipy.stats.rankdata(costs, axis=0)
-    tie_broken_nd_ranks = np.zeros_like(nd_ranks, dtype=np.int32)
-    for nd_rank in range(rank_lb, rank_ub):
-        mask = masks[nd_rank]
-        tie_break_ranks = _compute_rank_based_crowding_distance(ranks=ranks[mask])
+    (size, _) = ranks.shape
+    tie_broken_nd_ranks = np.zeros(size, dtype=np.int32)
+
+    for mask in nd_masks:
+        if avg_ranks is not None:
+            tie_break_ranks = scipy.stats.rankdata(avg_ranks[mask]).astype(np.int32)
+        else:  # Use crowding distance
+            tie_break_ranks = _compute_rank_based_crowding_distance(ranks=ranks[mask])
+
         # -1 to start tie_broken_nd_ranks from zero
         tie_broken_nd_ranks[mask] = tie_break_ranks + n_checked - 1
         n_checked += len(mask)
@@ -238,10 +195,18 @@ def _tie_break(
     tie_break: Literal["crowding_distance", "avg_rank"],
 ) -> np.ndarray:
     methods = ["crowding_distance", "avg_rank"]
+    ranks = scipy.stats.rankdata(costs, axis=0)
+    masks: List[List[int]] = [[] for _ in range(nd_ranks.max() + 1)]
+    for idx, nd_rank in enumerate(nd_ranks):
+        masks[nd_rank].append(idx)
+
     if tie_break == methods[0]:
-        return _tie_break_by_crowding_distance(costs, nd_ranks)
+        return _tie_break_by_method(nd_masks=masks, ranks=ranks)
     elif tie_break == methods[1]:
-        return _tie_break_by_avg_rank(costs, nd_ranks)
+        # min_ranks_factor plays a role when we tie-break same average ranks
+        min_ranks_factor = np.min(ranks, axis=-1) / (nd_ranks.size**2 + 1)
+        avg_ranks = np.mean(ranks, axis=-1) + min_ranks_factor
+        return _tie_break_by_method(nd_masks=masks, ranks=ranks, avg_ranks=avg_ranks)
     else:
         raise ValueError(f"tie_break method must be in {methods}, but got {tie_break}")
 
