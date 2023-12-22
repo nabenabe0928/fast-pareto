5f03cd80ab8ecc1b20f4d0d42ee724fe7f2e61a0
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2023-12-22 20:16:10 +0100

    Update mypy version

--- fast_pareto/__init__.py
+++ fast_pareto/__init__.py
@@ -2,7 +2,7 @@ from fast_pareto.pareto import is_pareto_front, is_pareto_front2d, nondominated_
 
 
 __version__ = "0.2.2"
-__copyright__ = "Copyright (C) 2022 Shuhei Watanabe"
+__copyright__ = "Copyright (C) 2023 Shuhei Watanabe"
 __licence__ = "Apache-2.0 License"
 __author__ = "Shuhei Watanabe"
 __author_email__ = "shuhei.watanabe.utokyo@gmail.com"
